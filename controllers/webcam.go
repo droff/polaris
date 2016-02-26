fc7c2602a80dfb5a982af48c0f7c4a2f50b2e7a4
droff <adroff@gmail.com>
2016-02-26 16:52:16 +0300

    :star:

--- controllers/webcam.go
+++ controllers/webcam.go
@@ -2,21 +2,15 @@ package controllers
 
 import (
 	"encoding/base64"
-	//"io/ioutil"
-	//"log"
-	"net/http"
-	//"os/exec"
 	"github.com/droff/camsnap"
+	"net/http"
 	"strconv"
 )
 
-// GetImage send image into stream
+// GetImage sends bas64/image into stream
 func GetImage(w http.ResponseWriter, r *http.Request) {
 	params := getParams(r)
 	if Session.Find(params["session_key"].(string)) {
-		//takeSnap()
-
-		//rawImage, _ := ioutil.ReadFile("/tmp/shot.jpg")
 		rawImage := camsnap.Get("/dev/video0", 1280, 720)
 		imageEncoded := base64.StdEncoding.EncodeToString(rawImage)
 
@@ -28,15 +22,3 @@ func GetImage(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusForbidden)
 	}
 }
-
-/*
-func takeSnap() {
-  //cmd := exec.Command("scrot", "/tmp/shot.jpg")
-  cmd := exec.Command("fswebcam", "-r", "1280x720", "--jpeg", "100", "--no-banner", "/tmp/shot.jpg")
-  cmd.Start()
-  err := cmd.Wait()
-  if err != nil {
-    log.Printf("snap ERROR: %v\n", err)
-  }
-}
-*/
