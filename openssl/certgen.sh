b24ad1886a45043dedace925b6782f00bbfea63f
droff <adroff@gmail.com>
2016-03-01 11:46:08 +0300

    sha256

--- openssl/certgen.sh
+++ openssl/certgen.sh
@@ -3,5 +3,5 @@
 openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
 openssl rsa -passin pass:x -in server.pass.key -out server.key
 rm server.pass.key
-openssl req -new -key server.key -out server.csr
+openssl req -new -sha256 -key server.key -out server.csr
 openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
