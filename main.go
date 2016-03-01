a18df8310cba6d97b5a9d46873bf08d543682f97
droff <adroff@gmail.com>
2016-03-01 11:28:32 +0300

    http2

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/droff/polaris/lib"
 	"github.com/droff/polaris/models"
+	"golang.org/x/net/http2"
 )
 
 func init() {
@@ -16,5 +17,13 @@ func main() {
 	router := lib.NewRouter()
 
 	log.Println("Listening...")
-	http.ListenAndServe(":4000", router)
+	/*http.ListenAndServe(":4000", router)*/
+
+	srv := &http.Server{
+		Addr:    ":4000",
+		Handler: router,
+	}
+
+	http2.ConfigureServer(srv, &http2.Server{})
+	srv.ListenAndServeTLS("openssl/server.crt", "openssl/server.key")
 }
