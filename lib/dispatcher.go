f502345baf71f35ef8cb6a55a92fe45d9b5e143a
droff <adroff@gmail.com>
2016-03-09 11:32:52 +0300

    remoteaddr nginx proxy pass

--- lib/dispatcher.go
+++ lib/dispatcher.go
@@ -12,6 +12,8 @@ var Params map[string]interface{}
 // Dispatcher func
 func Dispatcher(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r.RemoteAddr = r.Header.Get("X-Forwarded-For")
+
 		w.Header().Set("Server", "polaris")
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 
--- main.go
+++ main.go
@@ -17,14 +17,6 @@ func main() {
 	router := lib.NewRouter()
 	srvTLS := &http.Server{Addr: ":4000", Handler: router}
 
-	go func() {
-		srv := &http.Server{Addr: ":3000"}
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			http.Redirect(w, r, "https://0.0.0.0:4000"+r.RequestURI, http.StatusMovedPermanently)
-		})
-		srv.ListenAndServe()
-	}()
-
 	log.Println("Listening...")
 	http2.ConfigureServer(srvTLS, &http2.Server{})
 	srvTLS.ListenAndServeTLS("openssl/server.crt", "openssl/server.key")
